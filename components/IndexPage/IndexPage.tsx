b1201833758f0df41d6baa0025e8e825a5903634
Tristan Guest <tristanguest@gmail.com>
2023-08-28 10:50:37 -0300

    Update details

--- components/IndexPage/IndexPage.tsx
+++ components/IndexPage/IndexPage.tsx
@@ -21,6 +21,9 @@ export const IndexPage = () => {
           <div style={{ textAlign: "center" }}>
             <p>{"We're having a party!"}</p>
             <p>{"This is your invitation. RSVP below."}</p>
+            <p>
+              <b style={{ color: "red" }}>{"*Red = new information*"}</b>
+            </p>
             <GiDandelionFlower size={64} style={{ margin: 32 }} />
           </div>
           <section className={classes.details}>
@@ -29,7 +32,8 @@ export const IndexPage = () => {
                 Location
               </p>
               <p className={classes.description}>
-                {"2078 Highway 329, The Lodge, Nova Scotia (our place)"}
+                {"2078 Highway 329, The Lodge, Nova Scotia (our place). "}
+                <b style={{ color: "red" }}>Parking at 2118 Highway 329.</b>
               </p>
             </div>
             <div className={classes.info}>
@@ -50,8 +54,13 @@ export const IndexPage = () => {
               <p className={classNames(classes.heavy, classes.label)}>Wear</p>
               <p>
                 {
-                  "Whatever makes you feel good. We'll be taking the opportunity to wear something fun and fancy."
+                  "Whatever makes you feel good. We'll be taking the opportunity to wear something fun and fancy. "
                 }
+                <b style={{ color: "red" }}>
+                  {
+                    "Expect the ground to be a bit soft. You might have a hard time in pointy heels."
+                  }
+                </b>
               </p>
             </div>
             <div className={classes.info}>
@@ -61,14 +70,41 @@ export const IndexPage = () => {
               <p>
                 {
                   "There are some options within walking distance if you want a bed. Contact us about that. There's unlimited floor and tenting space."
-                }
+                }{" "}
+                <b style={{ color: "red" }}>
+                  {
+                    "If you're hoping to sleep under a roof, get in touch with us in advance."
+                  }
+                </b>
               </p>
             </div>
-            <p style={{ marginTop: "32px" }}>
+            <div className={classes.info}>
+              <p
+                className={classNames(classes.heavy, classes.label)}
+                style={{ color: "red" }}
+              >
+                Parking
+              </p>
+              <p style={{ color: "red" }}>
+                {
+                  "Most parking will be available at 2118 Highway 329 (Tristan's parents' place). From there you'll have to walk about 200 m through the woods to get to the venue. If you don't think you'll be up for the walk, please let us know, and we'll make a closer option available to you! There's a local cab service that will be available to get you around within the neighbourhood (Lucky Cab: 902 222-6213)."
+                }{" "}
+                <b style={{ color: "red" }}>
+                  {"Parking will be tight. Carpool if you can."}
+                </b>
+              </p>
+            </div>
+            <p style={{ marginTop: "32px", color: "red" }}>
               {
-                "We'll update this page as more details become clear, so check back here before you come. Also, parking is going to require a plan :). No gifts, please."
+                "We're super excited to see you on the 2nd. Get in touch with Laura or Tristan if you have questions about access or anything else."
               }
             </p>
+            <p style={{ color: "red" }}>
+              {
+                "If there's a story, anecdote, or something else you'd like to share publicly, there will be an opportunity."
+              }
+            </p>
+            <p>{"No gifts, please."}</p>
           </section>
           <GiDandelionFlower size={64} style={{ margin: 32 }} />
           <AttendeeForm />
